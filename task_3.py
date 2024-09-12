358cafd949e6f7599dc8286a75e5f04449e8cf33
Knurova Tatyana <tatiana.nikolaenko@inbox.ru>
2024-09-12 19:30:31 +0300

    change file

--- task_3.py
+++ task_3.py
@@ -1,55 +1,28 @@
-with open('1.txt', encoding='utf-8') as f1:
-            file1 = f1.readlines()
-with open('2.txt', encoding='utf-8') as f2:
-            file2 = f2.readlines()
-with open('3.txt', encoding='utf-8') as f3:
-            file3 = f3.readlines()
-with open('file.txt', 'w', encoding='utf-8') as f:
-    if len(file1) < len(file2) and len(file1) < len(file3):
-        f.write('1.txt' + '\n')
-        f.write(str(len(file1)) + '\n')
-        f.writelines(file1)
-        f.write('\n')
-    elif len(file2) < len(file1) and len(file2) < len(file3):
-        f.write('2.txt' + '\n')
-        f.write(str(len(file2)) + '\n')
-        f.writelines(file2)
-        f.write('\n')
-    elif len(file3) < len(file1) and len(file3) < len(file2):
-        f.write('3.txt' + '\n')
-        f.write(str(len(file3)) + '\n')
-        f.writelines(file3)
-        f.write('\n')
-    if len(file2) > len(file1) > len(file3) or len(file2) < len(file1) < len(file3):
-        f.write('1.txt' + '\n')
-        f.write(str(len(file1)) + '\n')
-        f.writelines(file1)
-        f.write('\n')
-    elif len(file1) > len(file2) > len(file3) or len(file2) > len(file1) and len(file2) > len(file3):
-        f.write('2.txt' + '\n')
-        f.write(str(len(file2)) + '\n')
-        f.writelines(file2)
-        f.write('\n')
-    elif len(file1) > len(file3) > len(file2) or \
-            len(file3) > len(file1) and len(file3) < len(file2):
-        f.write('3.txt' + '\n')
-        f.write(str(len(file3)) + '\n')
-        f.writelines(file3)
-        f.write('\n')
-    if len(file1) > len(file2) and len(file1) > len(file3):
-        f.write('1.txt' + '\n')
-        f.write(str(len(file1)) + '\n')
-        f.writelines(file1)
-        f.write('\n')
-    elif len(file2) > len(file1) and len(file2) > len(file3):
-        f.write('2.txt' + '\n')
-        f.write(str(len(file2)) + '\n')
-        f.writelines(file2)
-        f.write('\n')
-    elif len(file3) > len(file1) and len(file3) > len(file2):
-        f.write('3.txt' + '\n')
-        f.write(str(len(file3)) + '\n')
-        f.writelines(file3)
-        f.write('\n')
-    else:
-        print('Проверьте корректность данных')
+import os
+
+def process_files(file_names):
+    file_info = []
+    for file_name in file_names:
+        with open(file_name, encoding='utf-8') as f:
+            lines = f.readlines()
+            line_count = len(lines)
+            file_info.append((file_name, line_count, lines))
+    file_info.sort(key=lambda x: x[1])
+
+    file = os.path.abspath('C:/Users/tatia/PycharmProjects/pythonProject/file/task.txt')
+
+    with open(file, 'w', encoding='utf-8') as f_out:
+        for file_name, line_count, lines in file_info:
+            f_out.write(file_name + '\n')
+            f_out.write(str(line_count) + '\n')
+            f_out.writelines(lines)
+            f_out.write('\n')
+
+
+if __name__ == '__main__':
+    files_to_process = [
+        '1.txt',
+        '2.txt',
+        '3.txt'
+    ]
+    process_files(files_to_process)
